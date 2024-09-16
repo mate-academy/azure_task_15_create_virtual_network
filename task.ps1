692fc8ddfb9408ef8e68f280e3630430ac471b4a
Volkov <volkovyegorandriiovich@gmail.com>
2024-09-17 01:02:36 +0300

    required vnet created

--- task.ps1
+++ task.ps1
@@ -1,17 +1,41 @@
-$location = "uksouth"
-$resourceGroupName = "mate-azure-task-15"
+$location =                   "uksouth"
+$resourceGroupName =          "mate-azure-task-15"
 
-$virtualNetworkName = "todoapp"
-$vnetAddressPrefix = "10.20.30.0/24"
-$webSubnetName = "webservers"
-$webSubnetIpRange = "???" # <- calculate subnet ip address range 
-$dbSubnetName = "database"
-$dbSubnetIpRange = "???" # <- calculate subnet ip address range 
-$mngSubnetName = "management"
-$mngSubnetIpRange = "???" # <- calculate subnet ip address range 
+$virtualNetworkName =         "todoapp"
+$vnetAddressPrefix =          "10.20.30.0/24"
+
+$webSubnetName =              "webservers"
+$webSubnetIpRange =           "10.20.30.0/26"
+
+$dbSubnetName =               "database"
+$dbSubnetIpRange =            "10.20.30.64/26"
+
+$mngSubnetName =              "management"
+$mngSubnetIpRange =           "10.20.30.128/26"
 
 Write-Host "Creating a resource group $resourceGroupName ..."
-New-AzResourceGroup -Name $resourceGroupName -Location $location
+New-AzResourceGroup `
+  -Name                       $resourceGroupName `
+  -Location                   $location
 
-Write-Host "Creating a virtual network ..."
-# write your code here -> 
+Write-Host "Creating a virtual network $virtualNetworkName ..."
+$webSubnetConfig =  New-AzVirtualNetworkSubnetConfig `
+  -Name                       $webSubnetName `
+  -AddressPrefix              $webSubnetIpRange `
+
+$dbSubnetConfig =   New-AzVirtualNetworkSubnetConfig `
+  -Name                       $dbSubnetName `
+  -AddressPrefix              $dbSubnetIpRange `
+
+$mngSubnetConfig =  New-AzVirtualNetworkSubnetConfig `
+  -Name                       $mngSubnetName `
+  -AddressPrefix              $mngSubnetIpRange `
+
+New-AzVirtualNetwork `
+  -Name                       $virtualNetworkName `
+  -ResourceGroupName          $resourceGroupName `
+  -Location                   $location `
+  -AddressPrefix              $vnetAddressPrefix `
+  -Subnet                     $webSubnetConfig,`
+                              $dbSubnetConfig,`
+                              $mngSubnetConfig
