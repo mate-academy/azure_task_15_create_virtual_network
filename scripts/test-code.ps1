f78d2c9c30a9c0971757de54c99709ee806079c8
Beliar24 <blackdid7@gmail.com>
2025-04-25 21:16:55 +0300

    net

--- scripts/test-code.ps1
+++ scripts/test-code.ps1
@@ -1,19 +1,51 @@
 $scriptContent = Get-Content ./task.ps1
 
-if ($scriptContent | Where-Object {$_.ToLower().Contains("new-azresourcegroup")}) { 
-    Write-Host "Checking if script creates a resource group - ok" 
-} else { 
+if ($scriptContent | Where-Object {$_.ToLower().Contains("new-azresourcegroup")}) {
+    Write-Host "Checking if script creates a resource group - ok"
+} else {
     throw "Script is not creating a resource group, please review it. "
-} 
+}
 
-if ($scriptContent | Where-Object {$_.ToLower().Contains("new-azvirtualnetworksubnetconfig") -or $_.ToLower().Contains("add-azvirtualnetworksubnetconfig")}) { 
-    Write-Host "Checking if script creates subnets - ok" 
-} else { 
+if ($scriptContent | Where-Object {$_.ToLower().Contains("new-azvirtualnetworksubnetconfig") -or $_.ToLower().Contains("add-azvirtualnetworksubnetconfig")}) {
+    Write-Host "Checking if script creates subnets - ok"
+} else {
     throw "Script is not creating subnets, please review it. "
-} 
+}
 
-if ($scriptContent | Where-Object {$_.ToLower().Contains("new-azvirtualnetwork")}) { 
-    Write-Host "Checking if script creates a virtual network - ok" 
-} else { 
+if ($scriptContent | Where-Object {$_.ToLower().Contains("new-azvirtualnetwork")}) {
+    Write-Host "Checking if script creates a virtual network - ok"
+} else {
     throw "Script is not creating a virtual network, please review it. "
-} 
+}
+
+
+$vnet = @{
+    Name = 'todoapp'
+    ResourceGroupName = 'mate-azure-task-15'
+    Location = 'uksouth'
+    AddressPrefix = '10.20.30.0/24'
+}
+$virtualNetwork = New-AzVirtualNetwork @vnet
+
+$subnet = @{
+    Name = 'webservers'
+    VirtualNetwork = $virtualNetwork
+    AddressPrefix = '10.20.30.0/26'
+}
+Add-AzVirtualNetworkSubnetConfig @subnet
+
+$subnet = @{
+    Name = 'database'
+    VirtualNetwork = $virtualNetwork
+    AddressPrefix = '10.20.30.64/26'
+}
+Add-AzVirtualNetworkSubnetConfig @subnet
+
+$subnet = @{
+    Name = 'management'
+    VirtualNetwork = $virtualNetwork
+    AddressPrefix = '10.20.30.128/26'
+}
+Add-AzVirtualNetworkSubnetConfig @subnet
+
+$virtualNetwork | Set-AzVirtualNetwork
